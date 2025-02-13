7fd83dcfb72f8026a9513179035efd809c5ba1a5
Wasiu Ramoni <wasiu.a.ramoni@gmail.com>
2025-02-13 07:46:33 -0500

    Reactor: parse numerator and denominator

--- src/methods/simplify.ts
+++ src/methods/simplify.ts
@@ -2,8 +2,8 @@ import Fraction from '../core/Fraction';
 import { gcd } from '../core/gcd';
 
 export function simplify(fraction: Fraction): { numerator: number; denominator: number } {
-    const numerator = fraction.getNumerator();
-    const denominator = fraction.getDenominator();
+    const numerator = Number(fraction.getNumerator());
+    const denominator = Number(fraction.getDenominator());
     const commonDivisor = gcd(numerator, denominator);    
     return { numerator: numerator / commonDivisor, denominator: denominator / commonDivisor };
 }
