384f7b17e7109ce7029541c93cadd3389be6c137
Wasiu Ramoni <wasiu.a.ramoni@gmail.com>
2025-02-16 13:27:24 -0500

    Refactor: Update refactored get method

--- src/methods/toString.ts
+++ src/methods/toString.ts
@@ -1,14 +1,14 @@
 import Fraction from '../core/Fraction';
 
 export function toString(fraction: Fraction): string {
-  const numerator = fraction.getNumerator();
-  const denominator = fraction.getDenominator();
+  const numerator = fraction.getNumerator;
+  const denominator = fraction.getDenominator;
 
   // Handle whole numbers (e.g., 3)
   if (denominator === 1) {
     return `${numerator}`;
   }
-
-  // Handle improper fractions (e.g., 7/3)
+    // Handle improper fractions (e.g., 7/3)
+    // should be able to handle all sort and mixNumber
   return `${numerator}/${denominator}`;
 }
