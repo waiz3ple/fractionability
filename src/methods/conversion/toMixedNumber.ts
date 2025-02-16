b0747b597d780eb2189cf655a42e8dc6d74c2656
Wasiu Ramoni <wasiu.a.ramoni@gmail.com>
2025-02-16 12:50:06 -0500

    Reactor: Conversion is refactored to be used as standalone and chainable

--- src/methods/conversion/toMixedNumber.ts
+++ src/methods/conversion/toMixedNumber.ts
@@ -1,8 +1,15 @@
 import Fraction from '../../core/Fraction';
+import { toFraction } from '../../helper/toFraction';
+import { InputType } from '../../types/arithmetic.types';
+import { isProper } from '../comparison/isProper';
 
-export function toMixedNumber(fraction: Fraction): string {
-  const numerator = Number(fraction.getNumerator());
-  const denominator = Number(fraction.getDenominator());
+export function toMixedNumber(fraction: InputType): string {
+    const { getNumerator: numerator, getDenominator: denominator } = toFraction(fraction);
+    
+    if (isProper(fraction)) {
+       return new Fraction(numerator, denominator).toString()
+    }
+  
   const whole = Math.floor(numerator / denominator);
   const remainder = numerator % denominator;
   return remainder === 0 ? `${whole}` : `${whole} ${remainder}/${denominator}`;
