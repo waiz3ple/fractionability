7f6e68337e216a30b3b1b88614353cbdbd9092e3
Wasiu Ramoni <wasiu.a.ramoni@gmail.com>
2025-02-16 13:04:01 -0500

    Refactor: update get method

--- src/methods/simplify.ts
+++ src/methods/simplify.ts
@@ -1,9 +1,9 @@
 import Fraction from '../core/Fraction';
-import { gcd } from '../core/gcd';
+import { gcd } from '../helper/gcd';
 
 export function simplify(fraction: Fraction): { numerator: number; denominator: number } {
-    const numerator = fraction.getNumerator();
-    const denominator = fraction.getDenominator();
+    const numerator = fraction.getNumerator;
+    const denominator = fraction.getDenominator;
     const commonDivisor = gcd(numerator, denominator);    
     return { numerator: numerator / commonDivisor, denominator: denominator / commonDivisor };
 }
