c43ca4027a1afe911bc01df00f836906f11ae43d
Wasiu Ramoni <wasiu.a.ramoni@gmail.com>
2025-02-16 13:06:07 -0500

    Refactor: clean up toDecimal logic

--- src/methods/toDecimal.ts
+++ src/methods/toDecimal.ts
@@ -1,5 +1,5 @@
 import Fraction from '../core/Fraction';
 
 export function toDecimal(fraction: Fraction): number {
-  return Number(fraction.getNumerator()) / Number(fraction.getDenominator());
+  return fraction.getNumerator / fraction.getDenominator;
 }
