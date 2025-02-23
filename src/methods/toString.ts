5c0c375ff186c5a6259272bf1ad91ad128f48b2b
Wasiu Ramoni <wasiu.a.ramoni@gmail.com>
2025-02-23 10:59:55 -0500

    Refactor: toString to handle num==den scenerio

--- src/methods/toString.ts
+++ src/methods/toString.ts
@@ -8,6 +8,10 @@ export function toString(fraction: Fraction): string {
   if (denominator === 1) {
     return `${numerator}`;
   }
+  
+if(numerator == denominator){
+    return `1`;
+}
     // Handle improper fractions (e.g., 7/3)
     // should be able to handle all sort and mixNumber
   return `${numerator}/${denominator}`;
